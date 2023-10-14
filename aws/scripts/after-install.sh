261efd79abedb748900890ed800bb5426b5ea9da
Adams, Matthew <mcadams@shimadzu.com>
2023-10-14 14:44:05 -0400

    Added Amazon s3 bucket name and AWS region to deploy.yml and after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://##s3-bucket##/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://csc-swe-group3-stack-webappdeploymentbucket-1ta9b82ci7bd8/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
